f8259003218139e8d956f399244d5d99b4b19cdb
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-02-13 15:49:51 +0100

    working example

--- src/grab_subs.py
+++ src/grab_subs.py
@@ -6,4 +6,16 @@ config=dotenv_values(".env")
 
 API_KEY=config['API_KEY']
 
+MY_USERNAME=config['MY_USERNAME']
 
+MY_PASSWORD=config['MY_PASSWORD']
+
+
+subtitles = osc.OpenSubtitles("SubGrabber v1.0.0", API_KEY)
+
+subtitles.login(MY_USERNAME, MY_PASSWORD)
+response = subtitles.search(query="breaking bad", season_number=1, episode_number=1, languages="en")
+response_dict = response.to_dict()
+print(response_dict)
+srt = subtitles.download_and_save(response.data[0])
+print(srt)
