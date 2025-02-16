06807fd496b9705d659d3e4d65d727b82530c995
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-02-16 14:05:53 +0100

    improved code

--- src/grab_subs.py
+++ src/grab_subs.py
@@ -14,12 +14,6 @@ MY_PASSWORD=config['MY_PASSWORD']
 subtitles = osc.OpenSubtitles("SubGrabber v1.0.0", API_KEY)
 
 subtitles.login(MY_USERNAME, MY_PASSWORD)
-response = subtitles.search(query="breaking bad", season_number=1, episode_number=1, languages="en")
-response_dict = response.to_dict()
-print(response_dict)
-srt = subtitles.download(response.data[0])
-with open ("breaking_bad.srt", "wb") as f:
-    f.write(srt)
 
 def get_subs(query, season_number, episode_number, language, sub_name):
     response = subtitles.search(query=query, season_number=season_number, episode_number=episode_number, languages=language)
