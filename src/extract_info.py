51e725040af2977484cc750b2e2e0c91aca11aec
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-02-18 18:28:25 +0100

    added proper media info

--- src/extract_info.py
+++ src/extract_info.py
@@ -1,14 +1,25 @@
 # Description: Extracts media information from a file
+from re import I
 import regex as re
 
 class MediaInfo:
     def __init__(self, path):
         self.path = path
-        self.info = self._extract_info()
+        info= self._extract_info(path)
+        self.name = None if info['name'] is None else info['name']
+        self.season = None if info['season'] is None else info['season']
+        self.episode = None if info['episode'] is None else info['episode']
 
-    def _extract_info(self):
-        pattern = re.compile(r'(?P<name>.*?)(?P<ext>\..*?)$')
-        match = pattern.match(self.path)
-        if match:
-            return match.groupdict()
-        return None  
+    def _extract_info(self, pattern):
+        pattern = r'(?P<name>^[^S]*)(S(?P<season>\d+))?(E(?P<episode>\d+))?'
+        match = re.match(pattern, self.path)
+        return match.groupdict()
+
+    
+    def __str__(self):
+        return f"Name: {self.name}, Season: {self.season}, Episode: {self.episode}"
+
+if __name__ == "__main__":
+    path = "House_MD_S01E01.mkv"
+    media = MediaInfo(path)
+    print(media)
